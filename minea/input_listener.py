82fae70f873ca582892647853b813b9c680da8ec
keziah55 <19816994+keziah55@users.noreply.github.com>
2026-01-15 20:36:02 +0000

    adding comments

--- minea/input_listener.py
+++ minea/input_listener.py
@@ -4,11 +4,22 @@ import sys
 
 
 class InputListener(QObject):
+    """
+    Object that listens to stdin and send input via `received_input` signal.
+
+    Call `start()` to start listening to `stdin`. Call `stop()` directly to stop.
+
+    Parameters
+    ----------
+    loop_time
+        Number of milliseconds to block stdin and loop for. Default is 100ms.
+    """
 
     received_input = Signal(list)
 
-    def __init__(self, loop_time=100):
+    def __init__(self, loop_time: int = 100):
         super().__init__()
+
         self._run = False
         self._loop_time = loop_time / 1000
 
@@ -19,9 +30,12 @@ class InputListener(QObject):
 
     @Slot()
     def stop(self):
+        """Stop `stdin` loop."""
         self._run = False
 
     def start(self):
+        """Start `stdin` reading in loop."""
+
         self._run = True
 
         while self._run:
@@ -33,15 +47,27 @@ class InputListener(QObject):
 
 
 class InputController(QObject):
+    """
+    Object to manage `InputListener` in thread.
+
+    Received commands are send via the `received_input` signal.
+
+    Call `start()` to start listening to `stdin` and `stop()` to stop.
+
+    Parameters
+    ----------
+    loop_time_ms
+        Number of milliseconds to block stdin and loop for. Default is 100ms.
+    """
 
     received_input = Signal(list)
 
     _request_listener_stop = Signal()
 
-    def __init__(self):
+    def __init__(self, loop_time_ms: int = 100):
         super().__init__()
 
-        self._loop_time_ms = 100
+        self._loop_time_ms = loop_time_ms
 
         self._thread = QThread()
         self._listener = InputListener(loop_time=self._loop_time_ms)
@@ -55,13 +81,16 @@ class InputController(QObject):
         self._request_listener_stop.connect(self._listener.stop)
 
     def start(self):
+        """Start listening to stdin."""
         self._thread.start()
 
     def stop(self):
+        """Stop listening to stdin and quite thread."""
         self._listener.stop()
         self._thread.exit()
         self._thread.wait(self._loop_time_ms * 1.5)
 
     @Slot(list)
     def _received_input(self, cmd: list[str]):
+        """Send `received_input` signal."""
         self.received_input.emit(cmd)
